aca547f2e70db55ea53f374ca8d954de23f4c022
Max <gerasimovpost@gmail.com>
2022-05-22 19:20:27 +0400

    Creates navigation skeletton

--- components/SidebarNavigation/SidebarNavigation.tsx
+++ components/SidebarNavigation/SidebarNavigation.tsx
@@ -1,3 +1,30 @@
+import Link from "next/link";
+
+const menuItems = [
+  { text: "Projects", iconSrc: "/icons/projects.svg", href: "#" },
+  { text: "Issues", iconSrc: "/icons/issues.svg", href: "#" },
+  { text: "Alerts", iconSrc: "/icons/alert.svg", href: "#" },
+  { text: "Users", iconSrc: "/icons/users.svg", href: "#" },
+  { text: "Settings", iconSrc: "/icons/settings.svg", href: "#" },
+];
+
 export function SidebarNavigation() {
-  return <div>SidebarNavigation</div>;
+  return (
+    <nav>
+      <ul>
+        {menuItems.map(({ text, href }, index) => (
+          <li key={index}>
+            <Link href={href}>
+              <a>{text}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <ul>
+        <li>Support</li>
+        <li>Collapse</li>
+      </ul>
+    </nav>
+  );
 }
