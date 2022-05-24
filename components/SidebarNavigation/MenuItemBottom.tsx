c9b760ad57a83f7e53fbe415f775550d4d9232db
Max <gerasimovpost@gmail.com>
2022-05-24 18:21:33 +0400

    Adds normalize.css

--- components/SidebarNavigation/MenuItemBottom.tsx
+++ components/SidebarNavigation/MenuItemBottom.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import Link from "next/link";
 import styled from "styled-components";
 
 type MenuItemProps = {
   text: string;
-  href: string;
   iconSrc: string;
+  onClick: () => void;
 };
 
 const ListItem = styled.li`
   padding: 8px 12px;
 `;
 
-const Anchor = styled.a`
+const Button = styled.button`
   display: flex;
   align-items: center;
   color: #f2f4f7;
@@ -23,15 +22,13 @@ const Icon = styled.img`
   margin-right: 12px;
 `;
 
-export function MenuItem({ text, href, iconSrc }: MenuItemProps) {
+export function MenuItemButton({ text, onClick, iconSrc }: MenuItemProps) {
   return (
     <ListItem>
-      <Link href={href} passHref>
-        <Anchor>
-          <Icon src={iconSrc} alt={`${text} icon`} />
-          {text}
-        </Anchor>
-      </Link>
+      <Button onClick={onClick}>
+        <Icon src={iconSrc} alt={`${text} icon`} />
+        {text}
+      </Button>
     </ListItem>
   );
 }
---
+++ components/SidebarNavigation/MenuItemLink.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from "next/link";
+import styled from "styled-components";
+
+type MenuItemProps = {
+  text: string;
+  iconSrc: string;
+  href: string;
+};
+
+const ListItem = styled.li`
+  padding: 8px 12px;
+`;
+
+const Anchor = styled.a`
+  display: flex;
+  align-items: center;
+  color: #f2f4f7;
+  text-decoration: none;
+`;
+
+const Icon = styled.img`
+  margin-right: 12px;
+`;
+
+export function MenuItemLink({ text, href, iconSrc }: MenuItemProps) {
+  return (
+    <ListItem>
+      <Link href={href} passHref>
+        <Anchor>
+          <Icon src={iconSrc} alt={`${text} icon`} />
+          {text}
+        </Anchor>
+      </Link>
+    </ListItem>
+  );
+}
